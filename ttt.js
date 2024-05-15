eff764a33f21eec9a711d2c42bbbc0e2a899785c
allenhouu <126ahou@frhsd.com>
2024-05-15 14:05:13 -0400

    update

---
+++ ttt.js
@@ -0,0 +1,41 @@
+function clearGrid()
+{
+    document.getElementById("grid").innerHTML = "";
+}
+
+function space1()
+{
+
+}
+function space2()
+{
+
+}
+function space3()
+{
+
+}
+function space4()
+{
+
+}
+function space5()
+{
+
+}
+function space6()
+{
+
+}
+function space7()
+{
+
+}
+function space8()
+{
+
+}
+function space9()
+{
+
+}
