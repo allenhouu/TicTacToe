18b7543ff8cf15f6d28e9c5b401d1e09a396b291
allenhouu <126ahou@frhsd.com>
2024-05-20 14:04:28 -0400

    update

--- ttt.js
+++ ttt.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () =>
 {
-const cells = document.querySelectorAll(".cell");
+const cell = document.querySelectorAll(".cell");
 const status = document.querySelector("#status");
 const restart = document.querySelector("#restart");
+const singleP = document.querySelector("#singlePlayer");
+const twoP = document.querySelector("#twoPlayers");
 const winCondition = [
     [0,1,2], [3,4,5], [6,7,8],
     [0,3,6], [1,4,7], [2,5,8],
@@ -10,20 +12,26 @@ const winCondition = [
 ];
 let tiles = ["", "", "", "", "", "", "", "", ""];
 let currPlayer = 'X';
-let running = false
+let running = false;
+let isSinglePlayer = false;
+
+
 
 function startGame()
 {
-    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    cell.forEach(cell => cell.addEventListener("click", cellClicked));
     restart.addEventListener("click", restartGame);
     status.textContent = `${currPlayer}'s turn`;
     running = true;
 }
+function mode(isSinglePlayer)
+{
+
+}
 function cellClicked()
 {
     const cellIndex = this.getAttribute("cellIndex");
-    if (tiles[cellIndex] !== "" || !running)
-        return;
+    if (tiles[cellIndex] !== "" || !running) return;
     updateCell(this, cellIndex);
     checkWinner();
 }
@@ -41,10 +49,46 @@ function changePlayer()
 function restartGame()
 {
     currPlayer = 'X';
-
+    tiles = ["", "", "", "", "", "", "", "", ""];
+    status.textContent = `${currPlayer}'s turn`;
+    cell.forEach(cell => cell.textContent = "");
+    running = true;
 }
 function checkWinner()
 {
-    
+    let roundWon = winCondition.some(condition => {
+        const [a, b, c] = condition;
+        return tiles[a] && tiles[a] === tiles[b] && tiles [a] === tiles[c];
+    });
+
+    if (roundWon)
+    {
+        status.textContent = `${currPlayer} won`;
+        running = false;
+    }
+    else if(!tiles.includes(""))
+    {
+        status.textContent = "Draw";
+        running = false;
+    }
+    else
+    {
+        changePlayer();
+    }
 }
+function bestMove()
+{
+    let bestScore = -Infinity;
+    let move;
+    for (let i = 0; i < tiles.length; i++)
+    {
+        if (tiles[i] === "")
+        {
+            tiles[i] = "O";
+
+        }
+    }
+}
+
+startGame();
 });
