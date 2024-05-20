f2f212c8197a3efc359660fe0e3a296f92ac96be
allenhouu <126ahou@frhsd.com>
2024-05-20 07:23:48 -0400

    update

--- ttt.js
+++ ttt.js
@@ -1,47 +1,50 @@
-let board;
-let gameActive;
-
-function oneP()
+document.addEventListener("DOMContentLoaded", () =>
 {
+const cells = document.querySelectorAll(".cell");
+const status = document.querySelector("#status");
+const restart = document.querySelector("#restart");
+const winCondition = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+];
+let tiles = ["", "", "", "", "", "", "", "", ""];
+let currPlayer = 'X';
+let running = false
 
-}
-function twoP()
-{
-
-}
 function startGame()
 {
-    board =
-        [
-            ['', '', ''],
-            ['', '', ''],
-            ['', '', '']
-        ]
-    gameActive = true;
-    updateBoard();
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    restart.addEventListener("click", restartGame);
+    status.textContent = `${currPlayer}'s turn`;
+    running = true;
 }
-function updateBoard()
+function cellClicked()
 {
+    const cellIndex = this.getAttribute("cellIndex");
+    if (tiles[cellIndex] !== "" || !running)
+        return;
+    updateCell(this, cellIndex);
+    checkWinner();
+}
+
+function updateCell(cell, index)
+{
+    tiles[index] = currPlayer;
+    cell.textContent = currPlayer;
+}
+function changePlayer()
+{
+    currPlayer = currPlayer === 'X' ? 'O' : 'X';
+    status.textContent = `${currPlayer}'s turn`;
+}
+function restartGame()
+{
+    currPlayer = 'X';
 
 }
-function clearGrid()
-{
-    startGame();
-}
-function fullGrid()
+function checkWinner()
 {
     
 }
-function move(row, col)
-{
-        board[row][col] = 'X';
-        document.getElementById("grid-item").value = board;
-}
-function checkWin()
-{
-
-}
-document.addEventListener("DOMContentLoaded", () =>
-{
-    startGame();
 });
