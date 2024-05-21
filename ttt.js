85c28dd2f65ce04c62379c4b1beb73c9c7129113
allenhouu <126ahou@frhsd.com>
2024-05-21 13:51:46 -0400

    update

--- ttt.js
+++ ttt.js
@@ -8,14 +8,14 @@ const winCondition = [
     [0,3,6], [1,4,7], [2,5,8],
     [0,4,8], [2,4,6]
 ];
-let tiles = ["", "", "", "", "", "", "", "", ""];
+let tiles = Array(9).fill("");
 let currPlayer = 'X';
 let running = false;
 let isSinglePlayer = false;
 
 document.querySelector("#restart").addEventListener("click", restartGame);
-document.querySelector("#singlePlayer").addEventListener("click", selectMode(true));
-document.querySelector("#twoPlayers").addEventListener("click", selectMode(false));
+document.querySelector("#singlePlayer").addEventListener("click", () => selectMode(true));
+document.querySelector("#twoPlayers").addEventListener("click", () => selectMode(false));
 
 cell.forEach(cell => cell.addEventListener("click", cellClicked));
 function selectMode(singlePlayer)
@@ -29,9 +29,37 @@ function cellClicked()
     const cellIndex = this.getAttribute("cellIndex");
     if (tiles[cellIndex] !== "" || !running) return;
     updateCell(this, cellIndex);
-    checkWinner();
-}
 
+    if (checkWinner())
+    {
+        endStatus(`${currPlayer} wins!`);
+    }
+    else if(!tiles.includes(""))
+    {
+        endStatus(`Draw`)
+    }
+    else
+    {
+        changePlayer();
+        if (isSinglePlayer && currPlayer === 'O')
+        {
+            bestMove();
+            if (checkWinner())
+            {
+                endStatus(`${currPlayer} wins!`);
+            }
+            else
+            {
+                changePlayer();
+            }
+        }
+    }
+}
+function endStatus(msg)
+{
+    status.textContent = msg;
+    running = false;
+}
 function updateCell(cell, index)
 {
     tiles[index] = currPlayer;
@@ -52,25 +80,10 @@ function restartGame()
 }
 function checkWinner()
 {
-    let roundWon = winCondition.some(condition => {
+    return winCondition.some(condition => {
         const [a, b, c] = condition;
         return tiles[a] && tiles[a] === tiles[b] && tiles [a] === tiles[c];
     });
-
-    if (roundWon)
-    {
-        status.textContent = `${currPlayer} wins!`;
-        running = false;
-    }
-    else if(!tiles.includes(""))
-    {
-        status.textContent = "Draw";
-        running = false;
-    }
-    else
-    {
-        changePlayer();
-    }
 }
 function bestMove()
 {
@@ -81,13 +94,36 @@ function bestMove()
         if (tiles[i] === "")
         {
             tiles[i] = "O";
-
+            let score = minimax(tiles, 0, false);
+            tiles[i] = "";
+            if (score > bestScore)
+            {
+                bestScore = score;
+                move = i;
+            }
         }
     }
+    updateCell(cell[move], move);
 }
-function minimax()
+function minimax(board, depth, isMaximizing)
 {
+    if (checkWinner()) return isMaximizing ? -1 : 1;
+    if(!board.includes("")) return 0;
 
+    let bestScore = isMaximizing ? -Infinity : Infinity;
+    const currPlayer = isMaximizing ? 'O' : 'X';
+
+    for (let i = 0; i < board.length; i++)
+    {
+        if (board[i] === "")
+        {
+            board[i] = currPlayer;
+            let score = minimax(board, depth + 1, !isMaximizing);
+            board[i] = "";
+            bestScore = isMaximizing ? Math.max(score, bestScore) : Math.min (score, bestScore);
+        }
+    }
+    return bestScore;
 }
 
 restartGame();
